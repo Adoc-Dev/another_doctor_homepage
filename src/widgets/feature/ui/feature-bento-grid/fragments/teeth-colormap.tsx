e0779763f350ed89e51b33ebfca63ecbbf2e0e0d
nkcfe <chul.develop@gmail.com>
2025-09-05 10:33:46 +0900

    Fix: 이미지 수정

--- src/widgets/feature/ui/feature-bento-grid/fragments/teeth-colormap.tsx
+++ src/widgets/feature/ui/feature-bento-grid/fragments/teeth-colormap.tsx
@@ -5,8 +5,8 @@ function ColorMap() {
   return (
     <div className="relative inset-0 bg-white p-4 px-4">
       <Compare
-        firstImage="/images/teeth/teeth2.png"
-        secondImage="/images/teeth/teeth_color.png"
+        firstImage="/images/teeth/before.png"
+        secondImage="/images/teeth/after.png"
         firstImageClassName="object-cover "
         secondImageClassname="object-cover "
         className="h-[250px] w-full transition-all duration-300 ease-out group-hover:scale-105"
